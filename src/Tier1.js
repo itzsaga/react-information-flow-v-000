aceaa5c473b9a8a3dcb362b7894bb943dbf5598a
Seth Alexander <seth.alexander@wework.com>
2018-05-01 00:46:35 +0000

    automatically backed up by learn

--- src/Tier1.js
+++ src/Tier1.js
@@ -20,7 +20,7 @@ export default class Tier1 extends Component {
     return (
       <div onClick={() => {this.setState({color: "#000"})}} className="tier1" style={{backgroundColor: this.state.color, color: this.state.color}}>
         <Tier2 color={childColor} />
-        <Tier2 color={"#0FF"} />
+        <Tier2 color={childColor} />
       </div>
     )
   }
